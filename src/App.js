96ca376dde21e4c7251324975a389f5ea80b5966
someoneElse08 <basheer.ghlawanji@gmail.com>
2020-11-02 14:52:02 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import { useSelector, useDispatch} from 'react-redux';
+import { increment, decrement } from './action'
 
-function App() {
+
+
+function App(){
+  const counter = useSelector(state => state.counter)
+   const dispatch = useDispatch()
   return (
+   
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>{counter}</p> 
+        <button onClick={ () => dispatch(increment())}>+</button>
+        <button onClick={ () => dispatch(decrement())}>-</button>
+         <p>data should not display</p>
     </div>
   );
 }
+// const mapStatetoProps = state => {
+//   return {
+//     counter: state.counter
+//   }
+// }
+    
 
 export default App;
